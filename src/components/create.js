3e35a161858340669f98b695b2093e7de3f86c07
Olatunde <idowuolatunde32@gmail.com>
2022-07-22 22:58:18 +0100

    create.js updated

---
+++ src/components/create.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Button, Checkbox, Form } from 'semantic-ui-react'
+
+export default function Create() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [checkbox, setCheckbox] = useState(false);
+    return (
+        <div>
+            <Form className="create-form">
+                <Form.Field>
+                    <label>First Name</label>
+                    <input placeholder='First Name' />
+                </Form.Field>
+                <Form.Field>
+                    <label>Last Name</label>
+                    <input placeholder='Last Name' />
+                </Form.Field>
+                <Form.Field>
+                    <Checkbox label='I agree to the Terms and Conditions' />
+                </Form.Field>
+                <Button type='submit'>Submit</Button>
+            </Form>
+        </div>
+    )
+}
