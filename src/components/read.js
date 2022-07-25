5c40a215c66f4c42641a303077c811248781b5cf
Olatunde <idowuolatunde32@gmail.com>
2022-07-25 16:56:53 +0100

    update components updated

--- src/components/read.js
+++ src/components/read.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Table } from 'semantic-ui-react';
+import { Table, Button } from 'semantic-ui-react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 
 export default function Read() {
@@ -11,6 +12,16 @@ export default function Read() {
                 setAPIData(response.data);
             })
     }, [])
+
+    const setData = (data) => {
+        let { id, firstName, lastName, checkbox } = data;
+        localStorage.setItem('ID', id);
+        localStorage.setItem('First Name', firstName);
+        localStorage.setItem('Last Name', lastName);
+        localStorage.setItem('Checkbox Value', checkbox)
+    }
+    
+
     return (
         <div>
             <Table singleLine>
@@ -19,6 +30,7 @@ export default function Read() {
                         <Table.HeaderCell>First Name</Table.HeaderCell>
                         <Table.HeaderCell>Last Name</Table.HeaderCell>
                         <Table.HeaderCell>Checked</Table.HeaderCell>
+                        <Table.HeaderCell>Update</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
 
@@ -29,7 +41,13 @@ export default function Read() {
                             <Table.Cell>{data.firstName}</Table.Cell>
                             <Table.Cell>{data.lastName}</Table.Cell>
                             <Table.Cell>{data.checkbox ? 'Checked' : 'Unchecked'}</Table.Cell>
+                            <Link to =' /update'>
+                            <Table.Cell> 
+                                <Button onClick={() => setData(data)}>Update</Button>
+                            </Table.Cell>
+                        </Link>
                         </Table.Row>
+
                     )})}
                 </Table.Body>
             </Table>
--- src/components/update.js
+++ src/components/update.js
@@ -1,35 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import { Table, Button } from 'semantic-ui-react';
+import React, { useState, useEffect } from 'react';
+import { Button, Checkbox, Form } from 'semantic-ui-react'
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 
 
-export default function Read() {
-    const [APIData, setAPIData] = useState([]);
+export default function Update() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [checkbox, setCheckbox] = useState(false);
+    const [id, setID] = useState(null);
+
+    const updateAPIData = () => {
+        axios.put(`https://62dadaefe56f6d82a76b2d7e.mockapi.io/fakeData/${id}`, {
+            firstName,
+             lastName,
+             checkbox
+        })
+    }
+
     useEffect(() => {
-        axios.get(`https://62dadaefe56f6d82a76b2d7e.mockapi.io/fakeData`)
-            .then((response) => {
-                setAPIData(response.data);
-            })
-    }, [])
-   const setData = (data) => {
-   console.log(data);
-}
+            setID(localStorage.getItem('ID'))
+            setFirstName(localStorage.getItem('First Name'));
+            setLastName(localStorage.getItem('Last Name'));
+            setCheckbox(localStorage.getItem('Checkbox Value'))
+    }, []);
+
     return (
         <div>
-            <Table.HeaderCell>Update</Table.HeaderCell>
-            {APIData.map((data) => {
-            return (
-                
-                <Link to=' /update'>
-                    <Table.Cell> 
-                        <Button onClick={() => setData(data)}>Update</Button>
-                    </Table.Cell>
-                </Link>
-                        
-            )})}
-            
-            
+            <Form className="create-form">
+                <Form.Field>
+                    <label>First Name</label>
+                    <input placeholder='First Name' value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                </Form.Field>
+                <Form.Field>
+                    <label>Last Name</label>
+                    <input placeholder='Last Name' value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                </Form.Field>
+                <Form.Field>
+                    <Checkbox label='I agree to the Terms and Conditions' checked={checkbox} onChange={(e) => setCheckbox(!checkbox)} />
+                </Form.Field>
+                <Button type='submit' onClick={updateAPIData}>Update</Button>
+            </Form>
         </div>
     )
 }
