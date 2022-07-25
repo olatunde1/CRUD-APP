9495d95ce2eee349f20374d8131d81bc05d22aeb
Olatunde <idowuolatunde32@gmail.com>
2022-07-25 14:17:11 +0100

    read components added

--- src/components/create.js
+++ src/components/create.js
@@ -1,25 +1,35 @@
 import React, { useState } from 'react';
 import { Button, Checkbox, Form } from 'semantic-ui-react'
+import axios from 'axios';
+
 
 export default function Create() {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [checkbox, setCheckbox] = useState(false);
+    const postData = () => {
+        axios.post(`https://62dadaefe56f6d82a76b2d7e.mockapi.io/fakeData`, {
+            firstName,
+            lastName,
+            checkbox
+        })
+    }
+
     return (
         <div>
             <Form className="create-form">
                 <Form.Field>
                     <label>First Name</label>
-                    <input placeholder='First Name' />
+                    <input placeholder='First Name' onChange={(e) => setFirstName(e.target.value)} />
                 </Form.Field>
                 <Form.Field>
                     <label>Last Name</label>
-                    <input placeholder='Last Name' />
+                    <input placeholder='Last Name' onChange={(e) => setLastName(e.target.value)} />
                 </Form.Field>
                 <Form.Field>
-                    <Checkbox label='I agree to the Terms and Conditions' />
+                    <Checkbox label='I agree to the Terms and Conditions' onChange={(e) => setCheckbox(!checkbox)} />
                 </Form.Field>
-                <Button type='submit'>Submit</Button>
+                <Button onClick={postData} type='submit'>Submit</Button>
             </Form>
         </div>
     )
